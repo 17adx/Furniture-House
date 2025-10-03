9e968981609827d39d42bd0241e5b2ea523de247
Anas Salem <anas@alMacBook-Air-alkhas-bAnas.local>
2025-10-03 19:09:38 +0300

    Modified The Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/navbar/navbar';
+import Navbar from "../components/Navbar/navbar";
 import LoginSingUp from '../components/LoginSignUp/LoginSignUp';
 import Footer from '../components/Footer/footer';
 
